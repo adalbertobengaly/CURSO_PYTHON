55ffaee2232955e3cd28a15c5d116b6bee23c2d1
Adalberto Bengaly <adalbertobengaly@gmail.com>
2023-08-23 19:34:10 -0300

    Refatoração FizzBuzz e testFizzBuzz

--- Exercicios/fizzBuzz.py
+++ Exercicios/fizzBuzz.py
@@ -1,17 +1,29 @@
-def fizzBuzz(n):
-    rangeList = range(1, n + 1)
-    arrResult = []
-    for i in rangeList:
-        if i % 3 == 0 and i % 5 == 0:
-            arrResult.append("FizzBuzz")
-        elif i % 3 == 0:
-            arrResult.append("Fizz")
-        elif i % 5 == 0:
-            arrResult.append("Buzz")
-        else:
-            arrResult.append(i)
+def fizzBuzz(n: int):
+    if n % 3 == 0 and n % 5 == 0:
+        return "FizzBuzz"
+    elif n % 3 == 0:
+        return "Fizz"
+    elif n % 5 == 0:
+        return "Buzz"
+    else:
+        return n
 
-    return arrResult
+
+# def fizzBuzz2(n): # https://www.hackerrank.com/
+#     rangeList = range(1, n + 1)
+#     arrResult = []
+#     for i in rangeList:
+#         if i % 3 == 0 and i % 5 == 0:
+#             arrResult.append("FizzBuzz")
+#         elif i % 3 == 0:
+#             arrResult.append("Fizz")
+#         elif i % 5 == 0:
+#             arrResult.append("Buzz")
+#         else:
+#             arrResult.append(i)
+
+#     return arrResult
 
 if __name__ == "__main__":
-    print(fizzBuzz(15))
+    for n in range(3, 16):
+        print(fizzBuzz(n))
--- Exercicios/testFizzBuzz.py
+++ Exercicios/testFizzBuzz.py
@@ -4,12 +4,33 @@ from fizzBuzz import fizzBuzz
 class TestFizzBuzz(unittest.TestCase):
 
     def test_deve_retornar_FizzBuzz_se_entrada_multiplo_de_3_e_5(self):
-        esperado = [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz']
-        self.assertEqual(fizzBuzz(15), esperado)
+        entradas = (15, 30, 45, 60)
 
-    def test_deve_ret(self):
-        ...
+        for entrada in entradas:
+            with self.subTest(entrada = entrada):
+                self.assertEqual(fizzBuzz(entrada), "FizzBuzz")
+    
+    def test_deve_retornar_Fizz_se_entrada_multiplo_de_3_nao_5(self):
+        entradas = (3, 6, 9, 12, 18)
+
+        for entrada in entradas:
+            with self.subTest(entrada = entrada):
+                self.assertEqual(fizzBuzz(entrada), "Fizz")
+
+    def test_deve_retornar_Buzz_se_entrada_multiplo_de_5_nao_3(self):
+        entradas = (5, 10, 20, 25, 35)
+        
+        for entrada in entradas:
+            with self.subTest(entrada = entrada):
+                self.assertEqual(fizzBuzz(entrada), "Buzz")
+
+    def teste_deve_retornar_n_se_entrada_de_n_não_for_multiplo_de_3e_ou5(self):
+        entradas = (1, 11, 13, 16, 19)
+        
+        for entrada in entradas:
+            with self.subTest(entrada = entrada):
+                self.assertEqual(fizzBuzz(entrada), entrada)
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
         
