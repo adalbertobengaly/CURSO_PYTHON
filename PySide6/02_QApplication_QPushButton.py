830c1f29a4c467fb884bb0560156809bf0348763
Adalberto Bengaly <adalbertobengaly@gmail.com>
2023-11-05 13:12:58 -0300

    PySide6 - QApplication e QPushButton

---
+++ PySide6/02_QApplication_QPushButton.py
@@ -0,0 +1,17 @@
+# QApplication e QPushButton de PySide6.QtWidgets
+# QApplication -> O Widget principal da aplicação
+# QPushButton -> Um botão
+# PySide6.QtWidgets -> Onde estão os widgets do PySide6
+import sys
+from PySide6.QtWidgets import QApplication, QPushButton
+
+app = QApplication(sys.argv)
+
+botao = QPushButton('Texto do botão')
+botao.setStyleSheet('font-size: 40px; color: red;')
+botao.show()  # Adicionar o widget na heirarquia e exibe a janela
+
+# botao2 = QPushButton('Botão 2')
+# botao2.show()
+
+app.exec()  # o loop da aplicação
