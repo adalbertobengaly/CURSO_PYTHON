1a903800913a9db0574448279404ddbdb8aacc5f
adalbertobengaly <adalbertobengaly@gmail.com>
2023-11-13 23:08:32 -0300

    Usando placeholders para maior segurança (bindings) no SQLite

---
+++ BasesDeDados/02_sqlite3.py
@@ -0,0 +1,22 @@
+import sqlite3
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parent
+DB_NAME = 'db.sqlite3'
+DB_FILE = ROOT_DIR / DB_NAME
+TABLE_NAME = 'customers'
+
+connection = sqlite3.connect(DB_FILE)
+cursor = connection.cursor()
+
+sql = (
+    f'INSERT INTO {TABLE_NAME} '
+    '(name, weight) '
+    'VALUES '
+    '(?, ?)'
+)
+cursor.execute(sql, ('Joana', 4))
+connection.commit()
+
+cursor.close()
+connection.close()
