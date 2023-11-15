bd85fc655ebd5b8a424da0e20b250fb4de03b864
adalbertobengaly <adalbertobengaly@gmail.com>
2023-11-15 16:06:22 -0300

    Trocando o cursor para retornar dicionários - pymysql.cursors.DictCursor

---
+++ MySQL/cursors.py
@@ -0,0 +1,66 @@
+# PyMySQL - um cliente MySQL feito em Python Puro
+# Doc: https://pymysql.readthedocs.io/en/latest/
+# Pypy: https://pypi.org/project/pymysql/
+# GitHub: https://github.com/PyMySQL/PyMySQL
+import os
+
+import dotenv
+import pymysql
+import pymysql.cursors
+
+TABLE_NAME = 'customers'
+
+dotenv.load_dotenv()
+
+connection = pymysql.connect(
+    host=os.environ['MYSQL_HOST'],
+    user=os.environ['MYSQL_USER'],
+    password=os.environ['MYSQL_PASSWORD'],
+    database=os.environ['MYSQL_DATABASE'],
+    charset='utf8mb4',
+    cursorclass=pymysql.cursors.DictCursor,
+)
+
+with connection:
+    with connection.cursor() as cursor:
+        cursor.execute(f'TRUNCATE TABLE {TABLE_NAME}')
+        connection.commit()
+
+        sql = (
+            f'INSERT INTO {TABLE_NAME} '
+            '(nome, idade) VALUES '
+            '(%s, %s) '
+        )
+        valuesTuple = (
+            ("Adalberto", 26),
+            ("Natália", 25),
+            ("Siri", 22),
+            ("Helena", 15),
+            ("Jake", 32),
+            ("Anderson", 15),
+            ("Luiz", 23)
+        )
+        result = cursor.executemany(sql, valuesTuple)  # type:ignore
+        # print(sql)
+        # print(valuesTuple)
+        # print(result)
+    connection.commit()
+
+    with connection.cursor() as cursor:
+        sql = (
+            f'UPDATE {TABLE_NAME} '
+            'SET nome=%s, idade=%s '
+            'WHERE id = %s'
+        )
+
+        cursor.execute(sql, ('Eleonor', 102, 4))
+
+        cursor.execute(f'SELECT * FROM {TABLE_NAME} ')
+
+        # for row in cursor.fetchall():
+        #     _id, name, age = row.items()
+        #     print(_id, name, age)
+
+        for row in cursor.fetchall():
+            print(row)
+    connection.commit()
