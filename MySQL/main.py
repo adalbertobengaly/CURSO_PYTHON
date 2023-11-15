ba9411bce88ab22e271789026265cdcabe042a89
adalbertobengaly <adalbertobengaly@gmail.com>
2023-11-15 12:19:41 -0300

    TRUNCATE e INSERT p/ limpar e criar valores na tabela com um ou mais cursores

--- MySQL/main.py
+++ MySQL/main.py
@@ -7,22 +7,43 @@ import os
 import pymysql
 import dotenv
 
+TABLE_NAME = 'customers'
+
 dotenv.load_dotenv()
 
 connection = pymysql.connect(
     host=os.environ['MYSQL_HOST'],
     user=os.environ['MYSQL_USER'],
     password=os.environ['MYSQL_PASSWORD'],
-    database=os.environ['MYSQL_DATABASE']
+    database=os.environ['MYSQL_DATABASE'],
+    charset='utf8mb4'
 )
 with connection:
     with connection.cursor() as cursor:
         cursor.execute(
-            'CREATE TABLE IF NOT EXISTS customers ('
+            f'CREATE TABLE IF NOT EXISTS {TABLE_NAME} ('
             'id INT NOT NULL AUTO_INCREMENT, '
             'nome VARCHAR(50) NOT NULL, '
             'idade INT NOT NULL, '
             'PRIMARY KEY (id)'
             ')'
         )
-        connection.commit()  # Para criação de tabela não se faz necessário
+        cursor.execute(f'TRUNCATE TABLE {TABLE_NAME}')  # Limpa a tabela
+    connection.commit()  # Para criação de tabela não se faz necessário
+
+    # Começo a manipular dados a partir daqui
+    with connection.cursor() as cursor:
+        cursor.execute(
+            f'INSERT INTO {TABLE_NAME} '
+            '(nome, idade) VALUES ("Adalberto", 26)'
+        )
+        cursor.execute(
+            f'INSERT INTO {TABLE_NAME} '
+            '(nome, idade) VALUES ("Adalberto", 26)'
+        )
+        result = cursor.execute(
+            f'INSERT INTO {TABLE_NAME} '
+            '(nome, idade) VALUES ("Adalberto", 26)'
+        )
+        print(result)
+    connection.commit()
